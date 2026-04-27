bd72f328bd66cd6950808e3cced63eb55271810f
Saritha <saritha2k5@gmail.com>
2026-04-27 11:57:32 +0530

    Day11

--- April2026/src/main/java/Day6/TC_array1.java
+++ April2026/src/main/java/Day6/TC_array1.java
@@ -5,10 +5,17 @@ public class TC_array1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr1[];
-		arr1=new int[10];
+		int arr1[];   //declaration without size
+		arr1=new int[10]; //size is given here
 		int[] arr2= {1,2,3,4};
+		
+		
+		int[] arr3 = new int[] {30,50,70};
+		
+		
 		int twodim[][]=new int[2][2];
+		
+		
 
 	}
 
